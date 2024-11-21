aeab69181e5d3ca0e9bd529d14f5551786cf4277
Philip Nations <167041497+pnations@users.noreply.github.com>
2024-11-20 21:04:00 -0600

    add files via upload

---
+++ Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => (
+    <div className="loading-container">
+        <div className="loader">Loading...</div>
+    </div>
+);
+
+export default Loading;
